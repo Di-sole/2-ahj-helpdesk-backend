567e0f36383a4400de0a2a668dbe20e9d423219d
Olga Abramovich <disole@bk.ru>
2021-08-05 20:45:47 +0500

    tasck done

--- server.js
+++ server.js
@@ -58,24 +58,72 @@ const tickets = [
     name: 'Поменять краску в принтере',
     description: 'Кабинет 400, картридж на складе',
     status: 'false',
-    created: new Date(),
+    created: new Date().toLocaleString(),
   },
   {
     id: 2,
     name: 'Переустановить Windows',
     description: 'ПК в кабинете 200',
     status: 'false',
-    created: new Date(),
+    created: new Date().toLocaleString(),
+  },
+  {
+    id: 4,
+    name: 'Встретить курьера',
+    description: '12:00 у главного входа',
+    status: 'false',
+    created: new Date().toLocaleString(),
   }
 ];
 
 app.use(async ctx => {
-  ctx.response.body = tickets;
+  const { method, id } = ctx.request.query;
+
+  switch (method) {
+    case 'allTickets':
+      ctx.response.body = tickets;
+      return;
+    case 'ticketById':
+      const requiredTicket = tickets.find(o => o.id === parseInt(id));
+      ctx.response.body = requiredTicket;
+      return;
+    case 'createTicket':
+      const data = ctx.request.body;
+
+      if (data.id) {
+        const ticketToEdit = tickets.find(o => o.id === parseInt(id));
+        ticketToEdit.name = data.name;
+        ticketToEdit.description = data.description;
+        ticketToEdit.status = data.status;
+      } else {
+        const num = uuid.v4();
+        const newTicket = {
+          id: parseInt(num, 16),
+          name: data.name,
+          description: data.description,
+          status: 'false',
+          created: new Date().toLocaleString(),
+        };
+  
+        tickets.push(newTicket);
+      }
+
+      ctx.response.body = tickets;
+      return;
+    case 'deleteTicket':
+      const index = tickets.findIndex(o => o.id === parseInt(id));
+      tickets.splice(index,1);
+      ctx.response.body = tickets;
+      return;  
+    default:
+        ctx.response.status = 404;
+        return;
+  }
 });
 
 // app.use(async ctx => {
 //   console.log(ctx.request.query);
-//   ctx.response.body = 'servver response';
+//   ctx.response.body = 'server response';
 // });
 
 const port = process.env.PORT || 7070;
